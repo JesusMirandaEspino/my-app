834976a601f4e2e0b15b41e66ad5fe058ed1f23b
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-19 22:00:31 -0500

    hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ( { value  }  ) => {
+
+
+    const [ counter, SetCounter ] = useState( 0 );
+
+    const handleAdd = () => {
+        SetCounter( counter + 1 )
+    }
+
+
 
    // const saludo = 'Hola mundo desde react';
 
     return (
         <section className="fila">  
             <div className="contenedor1">
+
                 <div className="col-full-12">
                     <h1> CounterApp </h1>
                     <h2>{ value }</h2>
                 </div>
+
+                <div className="col-full-12">
+                    <h2>{ counter }</h2>
+                </div>
+
+                <div className="col-full-12">
+                    <button onClick={  handleAdd  } > +1 </button>
+                </div>
+    
             </div>
         </section>
     );
