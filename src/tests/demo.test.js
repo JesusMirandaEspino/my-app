d2e9a8eb165e6450e9f1ee02e86f921f39954d69
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-21 21:32:28 -0500

    primera prueba en react

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,7 +1,24 @@
+describe( 'Pruebas en el archivo demo test.js', () => { 
 
-test( 'PrimeraPrueba', () => { 
-    const isActive = true;
-    if( isActive ){
-        throw new Error( 'No esta activo' );
-    }
+
+    test( 'Primera Prueba valores deben ser iguales', () => { 
+
+        // 1.- Incializacion
+        const mensaje = 'Hola mundo';
+
+        //2.- Estimulo
+        const mensaje2 = `Hola mundo`;
+
+        //3.- Observar el comportamiento
+        expect( mensaje ).toBe( mensaje2 );
+
+    });
+
+
+
+
+
+
 } );
+
+
