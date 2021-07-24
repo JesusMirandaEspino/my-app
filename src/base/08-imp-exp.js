b711f91b2cd745ecc3405c20110a916b2d640de2
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-23 20:58:47 -0500

    test array

--- src/base/08-imp-exp.js
+++ src/base/08-imp-exp.js
@@ -1,20 +1,20 @@
 // import { heroes } from './data/heroes';
 // import { heroes } from './data/heroes';
 // import heroes, { owners } from '../data/heroes';
-import heroes from '../data/heroes';
+import  heroes  from '../data/heroes';
 
-// console.log( owners );
+// visualizar en consola console.log( owners );
 
 
 
 
 export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
-// console.log( getHeroeById(2) );
+// visualizar en consola console.log( getHeroeById(2) );
 
 // find?, filter
 export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
-// console.log( getHeroesByOwner('Marvel') );
+// visualizar en consola console.log( getHeroesByOwner('Marvel') );
 
 
