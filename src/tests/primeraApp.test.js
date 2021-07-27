c7ce31d43d677d97f2f0f41a1abd6b2d919f3364
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-26 20:33:31 -0500

    enzyme

--- src/tests/primeraApp.test.js
+++ src/tests/primeraApp.test.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react'
+//  Render import { render } from '@testing-library/react'
 
-
+import { shallow } from 'enzyme'
 import  PrimeraApp  from '../PrimeraApp';
+
+
 
 
 describe( 'Primera App', () => { 
 
+
+    /*  No se utlizara esta prueba 
     test( ' Debe de mostrar el mensa "Hola soy Goku"',  () => {
-
         const saludo = 'Hola, soy Goku';
+        const { getByText } = render( <PrimeraApp saludo={saludo} />);
+        expect( getByText(saludo) ).toBeInTheDocument(  );
+    });  */
 
-        const { getByText } = render( <PrimeraApp saludo="saludo" />);
 
-        expect( getByText(saludo) ).ToBeInTheDocument(  );
-        
-});
+    test( 'Debe de mostar <PrimeraApp/> correctamente', () => {
+
+        const saludo = 'Hola a todos';
+        const wrapper = shallow( <PrimeraApp saludo={ saludo } />);
+
+        expect( wrapper ).toMatchSnapshot(  );
+
+    });
+
+
 
 
 } );
-
